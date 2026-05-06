2a873ff16de3c82aae00eeb167f9d5d03bb79875
arv1z <ksskhramov@gmail.com>
2026-05-06 12:56:12 +0300

    lab5

---
+++ build/TEST/CMakeFiles/check.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check.dir/link.d"
+  "CMakeFiles/check.dir/test.cpp.o"
+  "CMakeFiles/check.dir/test.cpp.o.d"
+  "check"
+  "check.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/check.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/file/CMakeFiles/calc.dir/calc.cpp.o.d
@@ -0,0 +1,22 @@
+file/CMakeFiles/calc.dir/calc.cpp.o: /home/sasha/lab5/file/calc.cpp \
+ /usr/include/stdc-predef.h /home/sasha/lab5/file/calc.h \
+ /usr/include/c++/14/cstdint \
+ /usr/include/x86_64-linux-gnu/c++/14/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/14/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/14/bits/cpu_defines.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h
